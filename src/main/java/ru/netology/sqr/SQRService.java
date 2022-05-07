7f8f31e2c0377f586785abcbc6ab5d29a408586b
Plushcake <Nikkolo.Makeawelli@gmail.com>
2022-05-07 14:31:54 +0300

    Added folde  SRC

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int specifiedRange(int lowBorder, int highBorder) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= lowBorder && i * i <= highBorder) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+
